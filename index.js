e1c556d2da63628bd75b6836712d1179ce8e7190
OnanThairu <thairuonan@gmail.com>
2022-06-29 13:14:44 +0300

    myMap

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function myEach(collection, callback) {
-    const arr = Object.values(collection)
-    for (const elem of arr) {
+    const array = Object.values(collection)
+    for (const elem of array) {
         callback(elem)
     }
     return collection
+}
+
+function myMap(collection, callback) {
+    const array = Object.values(collection)
+    const newArray = []
+    for (const elem of array) {
+        newArray.push(callback(elem))
+    }
+    return newArray
 }
